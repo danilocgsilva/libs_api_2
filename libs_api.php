c3ad3809fe12e630c59b343e131b6de0ae8547ea
Danilo Silva <Danilo Silva>
2016-10-19 10:01:27 +0100

    First addings

---
+++ libs_api.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Libs API
+Description: Automatically adds javascript and css libraries.
+Plugin URI: danilocgsilva.me
+Author: Danilo Silva (danilocgsilva)
+Author URI: danilocgsilva
+License: GPLv2
+*/
